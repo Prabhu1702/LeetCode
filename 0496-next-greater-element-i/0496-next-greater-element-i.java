3620c800d7b1836b3a04d640bd94e7058ef62b8e
Prabhu1702 <156276905+Prabhu1702@users.noreply.github.com>
2024-09-13 13:55:12 +0530

    Time: 3 ms (93.15%), Space: 43.9 MB (40.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,3 +1,5 @@
+//in this code we have created 2 dataset one hash map and one stack. what we are doing is pushing element from nums2 into stack and when we push other element we check if its greater than the existing element (we pop the existing element and the num we are currently pushing) together into the hasp map or we just push the element into the stakck. after full iteration we check if stack is empty or not if not we push the stack element and -1 together into the map. after this we check the elements in nums1 and check corresponding value of the element in hashmap.
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map <Integer,Integer> greater=new HashMap<>();
