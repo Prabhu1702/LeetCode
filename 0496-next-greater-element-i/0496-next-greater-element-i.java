dabef473be97028b485ea4535f3d6d27aae061ad
Prabhu1702 <156276905+Prabhu1702@users.noreply.github.com>
2025-07-03 14:13:28 +0530

    Time: 3 ms (92.64%), Space: 44 MB (50.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,24 @@
-//in this code we have created 2 dataset one hash map and one stack. what we are doing is pushing element from nums2 into stack and when we push other element we check if its greater than the existing element (we pop the existing element and the num we are currently pushing) together into the hasp map or we just push the element into the stakck. after full iteration we check if stack is empty or not if not we push the stack element and -1 together into the map. after this we check the elements in nums1 and check corresponding value of the element in hashmap.
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map <Integer,Integer> greater=new HashMap<>();
-        Stack<Integer> stack=new Stack<>();
+        Stack<Integer> stack= new Stack<>();
+        HashMap<Integer,Integer> nextGreater= new HashMap<>();
 
-        for(int nums:nums2){
-
-            while(!stack.isEmpty() && nums>stack.peek()){
-                greater.put(stack.pop(),nums);
-            }
-
-            stack.push(nums);
-        }
-         while (!stack.isEmpty()) {
-            greater.put(stack.pop(), -1);
+        for(int num: nums2){
+        while(!stack.isEmpty() && num> stack.peek()){
+            nextGreater.put(stack.pop(), num);
         }
 
-        int [] result=new int[nums1.length] ;
-        
-        for(int i=0;i<nums1.length;i++)
-        {
-            result[i]=greater.get(nums1[i]);
-        }
-        return result;
-           }
+         stack.push(num);
+    }
+
+    while(!stack.isEmpty()){
+        nextGreater.put(stack.pop(),-1);
+    }
+
+    int[] result = new int[nums1.length];
+    for(int i =0;i<nums1.length;i++){
+            result[i]=nextGreater.get(nums1[i]);
+    }
+    return result;
+}
 }
