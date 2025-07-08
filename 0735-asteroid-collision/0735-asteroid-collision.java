28b4103aa640adf3cb98c4d0cb1ad7ea76377556
Prabhu1702 <156276905+Prabhu1702@users.noreply.github.com>
2025-07-08 11:52:30 +0530

    Time: 6 ms (65.16%), Space: 45.4 MB (53.23%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,43 +1,39 @@
-import java.util.Stack;
-
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack=new Stack<>();
 
-        for (int asteroid : asteroids) {
-            // Assume the asteroid will be added to the stack unless it's destroyed
-            boolean destroyed = false;
+        for(int a: asteroids){
+            boolean destroyed=false;
 
-            // Check for a collision scenario: stack's top is positive and current asteroid is negative
-            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
-                // Compare absolute values for collision resolution
-                if (stack.peek() < -asteroid) {
-                    // Current left-moving asteroid is larger, destroy the top of the stack
+            while(!stack.isEmpty() && a<0 && stack.peek()>0){
+                int top=stack.peek();
+
+                if(top< -a){
                     stack.pop();
-                } else if (stack.peek() == -asteroid) {
-                    // Both asteroids are of equal size, destroy both
+                }
+
+                else if(top == -a){
+                    destroyed=true;
                     stack.pop();
-                    destroyed = true;
                     break;
-                } else {
-                    // Stack's top is larger, current asteroid is destroyed
-                    destroyed = true;
+                }
+                
+                else{
+                    destroyed=true;
                     break;
                 }
             }
-
-            // If the current asteroid wasn't destroyed, push it to the stack
-            if (!destroyed) {
-                stack.push(asteroid);
+            if(!destroyed){
+                stack.push(a);
             }
         }
 
-        // Convert the remaining stack to an array
-        int[] result = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
+            int []result= new int[stack.size()];
+            for(int i=result.length-1;i>=0;i--){
+                result[i]=stack.pop();
+            }
 
-        return result;
+            return result;
+
     }
 }
